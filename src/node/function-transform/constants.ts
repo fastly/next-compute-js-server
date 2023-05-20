73187a6f7c7f720745f5bb11a500bd9b3341573c
Katsuyuki Omuro <komuro@fastly.com>
2023-05-20 09:24:29 +0900

    Add compatibility for 13.0.1 and 13.0.2

--- src/node/function-transform/constants.ts
+++ src/node/function-transform/constants.ts
@@ -24,4 +24,6 @@ export const VERCEL_FUNCTION_CONFIG_FILENAME = '.vc-config.json';
 export const NEXT_VERSION = '13.0.0';
 export const COMPATIBLE_NEXT_VERSIONS = [
   '13.0.0',
+  '13.0.1',
+  '13.0.2',
 ];
