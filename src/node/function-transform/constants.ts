ea4f4114cc6a9dfea5202117a06bb464eb5464b1
Katsuyuki Omuro <komuro@fastly.com>
2023-05-23 00:25:20 +0900

    Create 13.2.2 branch

--- src/node/function-transform/constants.ts
+++ src/node/function-transform/constants.ts
@@ -21,8 +21,7 @@ export type VcConfig =
 
 export const VERCEL_FUNCTION_CONFIG_FILENAME = '.vc-config.json';
 
-export const NEXT_VERSION = '13.2.0';
+export const NEXT_VERSION = '13.2.2';
 export const COMPATIBLE_NEXT_VERSIONS = [
-  '13.2.0',
-  '13.2.1',
+  '13.2.2',
 ];
