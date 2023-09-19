3a50c615ea1c7bfadcfa723dcb46b932cb73d4e8
Katsuyuki Omuro <komuro@fastly.com>
2023-09-19 15:45:14 +0900

    Reset next-13.2.2

--- src/node/function-transform/constants.ts
+++ src/node/function-transform/constants.ts
@@ -26,7 +26,8 @@ export type VcConfig =
 
 export const VERCEL_FUNCTION_CONFIG_FILENAME = '.vc-config.json';
 
-export const NEXT_VERSION = '12.3.0';
+export const NEXT_VERSION = '13.2.2';
 export const COMPATIBLE_NEXT_VERSIONS = [
-  '12.3.0'
+  '13.2.2',
+  '13.2.3',
 ];
